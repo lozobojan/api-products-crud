854705488f30c22f3bdfebebb7bdb70c8a81715f
Bojan Lozo <bojanlo@maestralsolutions.com>
2023-10-05 10:48:59 +0200

    - Authorized user actions via policy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
 use App\Models\Product;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
@@ -13,41 +14,51 @@ class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
+     * @throws AuthorizationException
      */
     public function index(): Collection
     {
+        $this->authorize('viewAny', Product::class);
         return Product::all();
     }
 
     /**
      * Store a newly created resource in storage.
+     * @throws AuthorizationException
      */
     public function store(StoreProductRequest $request): Model|Builder
     {
+        $this->authorize('create', Product::class);
         return Product::query()->create($request->validated());
     }
 
     /**
      * Display the specified resource.
+     * @throws AuthorizationException
      */
     public function show(Product $product): Product
     {
+        $this->authorize('view', $product);
         return $product;
     }
 
     /**
      * Update the specified resource in storage.
+     * @throws AuthorizationException
      */
     public function update(UpdateProductRequest $request, Product $product): int
     {
+        $this->authorize('update', $product);
         return Product::query()->update($request->validated());
     }
 
     /**
      * Remove the specified resource from storage.
+     * @throws AuthorizationException
      */
     public function destroy(Product $product): ?bool
     {
+        $this->authorize('delete', $product);
         return $product->delete();
     }
 }
--- app/Policies/ProductPolicy.php
+++ app/Policies/ProductPolicy.php
@@ -11,7 +11,7 @@ class ProductPolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(?User $user): bool
     {
         return true;
     }
@@ -19,7 +19,7 @@ class ProductPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Product $product): bool
+    public function view(?User $user, Product $product): bool
     {
         return true;
     }
@@ -48,19 +48,4 @@ class ProductPolicy
         return $user->is_admin;
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Product $product): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Product $product): bool
-    {
-        return false;
-    }
 }
